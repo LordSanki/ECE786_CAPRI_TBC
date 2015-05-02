f85377957d7a17fd4feaecd72d429d50cc38feb8
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2015-05-01 22:39:53 -0400

    fixing yet another issue in script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt-get install build-essential
 if [$(uname -m | grep '64') != ""];
 then
   wget http://developer.download.nvidia.com/compute/cuda/4_0/toolkit/cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
-  sudo chmod u+x downloads/cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
+  sudo chmod u+x cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
   echo "\n\n\nInstalling cuda. Press Enter when prompted for path\n\n"
   sudo ./cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
 else
