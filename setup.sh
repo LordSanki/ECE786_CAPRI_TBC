6e06ea9985242c6e2a76d0ef1fd641004b961faa
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2015-05-01 22:01:51 -0400

    fixing another issue in script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
-mkdir -p downloads; cd downloads;
-
-if $(uname -m | grep '64'); then
+if $(uname -m | grep '64') then
   wget http://developer.download.nvidia.com/compute/cuda/4_0/toolkit/cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
-  sudo chmod u+x cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
+  sudo chmod u+x downloads/cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
   echo "\n\n\nInstalling cuda. Press Enter when prompted for path\n\n"
   sudo ./cudatoolkit_4.0.17_linux_64_ubuntu10.10.run
 else
-  wget http://developer.download.nvidia.com/compute/cuda/4_0/toolkit/cudatoolkit_4.0.17_linux_32_ubuntu10.10.run
+  wget http://developer.download.nvidia.com/compute/cuda/4_0/toolkit/cudatoolkit_4.0.17_linux_32_ubuntu10.10.run 
   sudo chmod u+x cudatoolkit_4.0.17_linux_32_ubuntu10.10.run
   echo "\n\n\nInstalling cuda. Press Enter when prompted for path\n\n"
   sudo ./cudatoolkit_4.0.17_linux_32_ubuntu10.10.run
 fi
+sudo apt-get install xutils-dev
 sudo apt-get install gcc-4.4
 sudo apt-get install g++-4.4
 cd ../
 
 git clone https://github.com/LordSanki/gpgpu-sim_distribution.git gpgpusim
 cd gpgpusim
-export CUDA_INSTALL_PATH=/usr/local/cuda
+echo "export CUDA_INSTALL_PATH=/usr/local/cuda" >> setup_env
+cat setup_environment >> setup_env
+mv setup_env setup_environment
 source setup_environment > /dev/null
 make > /dev/null
 cd ../
