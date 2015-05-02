9ac886e95d8f2f6addaa849d14cd2277161e95f4
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2015-05-01 22:51:09 -0400

    renaming file as trace.h already exists in gpgpusim

---
+++ capri_trace.h
@@ -0,0 +1,41 @@
+#ifndef __CAPRI_TRACE_H__
+#define __CAPRI_TRACE_H__
+
+#include <vector>
+#include <bitset>
+
+enum Operands
+{
+  NO_OP=-1,
+  ALU_OP=1,
+  SFU_OP,
+  ALU_SFU_OP,
+  LOAD_OP,
+  STORE_OP,
+  BRANCH_OP,
+  BARRIER_OP,
+  MEMORY_BARRIER_OP,
+  CALL_OPS,
+  RET_OPS
+};
+
+struct Instruction
+{
+  Operands op;
+  int pc;
+  bitset<32> mask;
+};
+
+struct Warps
+{
+  std::vector<Instruction> vIns;
+};
+
+struct ThreadBlock
+{
+  std::vector<Warps> vWarps;
+};
+
+
+#endif //__CAPRI_TRACE_H__
+
