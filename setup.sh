84c885769a734df1627563a0033c30475ecd8064
Prashant Solanki <psolank@ncsu.edu>
2015-05-03 00:03:29 -0400

    yet more changes

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ then
   then
     echo "direcoty $PWD/cuda exists skipping installation. Remove it to reinstall"
   else
-    eval "mkdir cuda; chmod u+x cudatoolkit_4.0.17_linux_64_ubuntu10.10.run; ./cudatoolkit_4.0.17_linux_64_ubuntu10.10.run -- -prefix=$PWD/cuda"
+    bash -lic "mkdir cuda; chmod u+x cudatoolkit_4.0.17_linux_64_ubuntu10.10.run; ./cudatoolkit_4.0.17_linux_64_ubuntu10.10.run -- -prefix=$PWD/cuda"
   fi
 else
   dfile=$PWD/cudatoolkit_4.0.17_linux_32_ubuntu10.10.run
@@ -25,7 +25,7 @@ else
   then
     echo "direcoty $PWD/cuda exists skipping installation. Remove it to reinstall"
   else
-    eval "mkdir cuda; chmod u+x cudatoolkit_4.0.17_linux_32_ubuntu10.10.run; ./cudatoolkit_4.0.17_linux_32_ubuntu10.10.run -- -prefix=$PWD/cuda"
+    bash -lic "mkdir cuda; chmod u+x cudatoolkit_4.0.17_linux_32_ubuntu10.10.run; ./cudatoolkit_4.0.17_linux_32_ubuntu10.10.run -- -prefix=$PWD/cuda"
   fi
 fi
 
@@ -40,18 +40,18 @@ sudo apt-get install libz-dev
 ln -sf /usr/bin/gcc-4.4 gcc
 ln -sf /usr/bin/g++-4.4 g++
 
-if [ ! -d "$gpgpusim" ];
+if [ -d "gpgpusim" ];
 then
+  bash -lic "cd gpgpusim; git pull;"
+else
   git clone https://github.com/LordSanki/gpgpu-sim_distribution.git gpgpusim
   cp gpgpusim/setup_environment gpgpusim/setup_environment.back
-else
-  eval "cd gpgpusim; git pull;"
 fi
 
 echo "export CUDA_INSTALL_PATH=$PWD/../cuda" > gpgpusim/setup_environment
 echo "export PATH=$PWD/../:$PATH" >> gpgpusim/setup_environment
 cat gpgpusim/setup_environment.back >> gpgpusim/setup_environment
-eval "cd gpgpusim; source setup_environment; make;"
+bash -lic "cd gpgpusim; source setup_environment; make;"
 
 echo "export PATH=$PWD:$PATH" > source_me
 echo "export LD_LIBRARY_PATH=$PWD/gpgpusim/lib/"$(ls $PWD/gpgpusim/lib/)"/cuda-4000/release:$LD_LIBRARY_PATH" >>source_me
